4acb8aaf38c4a368d27deef6ab51d51a2fd9fba3
Michael ndolo <michaelndolo17142@gmail.com>
2024-03-25 08:40:12 +0300

    few updates

--- index.php
+++ index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    
+    <nav>
+        Welcome to freeze flames entertainment
+    </nav>
+
+    <form class="signin">
+        <input type="email" placeholder="email">
+        <input type="password" placeholder="password">
+        <input type="submit">
+    </form>
 </body>
 </html>
